30c96e4cc3c37aa9b51705dec62c181a3bb05970
gongdanyu <gongdanyu@baidu.com>
2015-09-08 11:45:38 +0800

    Initial Commit

---
+++ CircleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleView
+//
+//  Created by danyu on 15/9/8.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleView
+//
+//  Created by danyu on 15/9/8.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
